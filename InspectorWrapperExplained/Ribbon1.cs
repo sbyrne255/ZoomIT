b3fb3e9df2dfeb878efdb6aba95918e61b0f0281
Steven Byrne <Stevenbyrne255@gmail.com>
2017-08-10 16:49:58 -0500

    V2.4.1 First Public Commit
    First public commit including all source files and installers.

---
+++ InspectorWrapperExplained/Ribbon1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using Office = Microsoft.Office.Core;
+using System.Windows.Forms;
+using System.Drawing;
+using InspectorWrapperExplained.Properties;
+
+// TODO:  Follow these steps to enable the Ribbon (XML) item:
+
+// 1: Copy the following code block into the ThisAddin, ThisWorkbook, or ThisDocument class.
+
+//  protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+//  {
+//      return new Ribbon1();
+//  }
+
+// 2. Create callback methods in the "Ribbon Callbacks" region of this class to handle user
+//    actions, such as clicking a button. Note: if you have exported this Ribbon from the Ribbon designer,
+//    move your code from the event handlers to the callback methods and modify the code to work with the
+//    Ribbon extensibility (RibbonX) programming model.
+
+// 3. Assign attributes to the control tags in the Ribbon XML file to identify the appropriate callback methods in your code.  
+
+// For more information, see the Ribbon XML documentation in the Visual Studio Tools for Office Help.
+
+
+namespace InspectorWrapperExplained
+{
+    [ComVisible(true)]
+    public class Ribbon1 : Office.IRibbonExtensibility
+    {
+        private Office.IRibbonUI ribbon;
+
+        public Ribbon1()
+        {
+        }
+
+        private string x = "";
+        public Bitmap imageSuper_GetImage(Office.IRibbonControl control)
+        {
+            try
+            {
+                return Resources.Zoom_in_512;
+            }
+            catch (Exception er) { MessageBox.Show(er.ToString()); return null; }
+        }
+        #region IRibbonExtensibility Members
+        public void Button_Click(Office.IRibbonControl control)
+        {
+            try
+            {
+                if (x.Length <= 0)
+                {
+                    Properties.Settings.Default.zoomLevel = 150;
+                    Properties.Settings.Default.Save();
+                }
+                else
+                {
+                    int q = Convert.ToInt32(x);
+
+                    if (q > 500)
+                    {
+                        Properties.Settings.Default.zoomLevel = 500;
+                        Properties.Settings.Default.Save();
+                        return;
+                    }
+                    if (q < 10)
+                    {
+                        Properties.Settings.Default.zoomLevel = 10;
+                        Properties.Settings.Default.Save();
+                        return;
+                    }
+                    else
+                    {
+                        Properties.Settings.Default.zoomLevel = q;
+                        Properties.Settings.Default.Save();
+                    }
+                }
+            }
+            catch (Exception) { MessageBox.Show("Please only enter numbers. \n"); }
+        }
+        public void RecupDonnee(Office.IRibbonControl control, String text)
+        {
+            try
+            {
+                x = text;
+            }
+            catch (Exception er) { MessageBox.Show(er.ToString()); }
+
+        }
+        #endregion
+
+        #region IRibbonExtensibility Members
+
+        public string GetCustomUI(string ribbonID)
+        {
+            return GetResourceText("InspectorWrapperExplained.Ribbon1.xml");
+        }
+
+        #endregion
+
+        #region Ribbon Callbacks
+        //Create callback methods here. For more information about adding callback methods, select the Ribbon XML item in Solution Explorer and then press F1
+
+        public void Ribbon_Load(Office.IRibbonUI ribbonUI)
+        {
+            this.ribbon = ribbonUI;
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private static string GetResourceText(string resourceName)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            string[] resourceNames = asm.GetManifestResourceNames();
+            for (int i = 0; i < resourceNames.Length; ++i)
+            {
+                if (string.Compare(resourceName, resourceNames[i], StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    using (StreamReader resourceReader = new StreamReader(asm.GetManifestResourceStream(resourceNames[i])))
+                    {
+                        if (resourceReader != null)
+                        {
+                            return resourceReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        #endregion
+    }
+}
---
+++ InspectorWrapperExplained/bin/Release/ThisAddIn.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using System.Windows.Forms;
+
+namespace InspectorWrapperExplained
+{
+    public partial class ThisAddIn
+    {
+
+        /// <summary>
+        /// Holds a reference to the Application.Inspectors collection
+        /// Required to get notifications for NewInspector events.
+        /// </summary>
+        private Outlook.Inspectors _inspectors;
+        protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+        {
+            return new Ribbon1();
+        }
+        /// <summary>
+        /// A dictionary that holds a reference to the Inspectors handled by the add-in
+        /// </summary>
+        private Dictionary<Guid, InspectorWrapper> _wrappedInspectors;
+
+        /// <summary>
+        /// Startup method is called when the add-in is loaded by Outlook
+        /// </summary>
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            try
+            {
+                _wrappedInspectors = new Dictionary<Guid, InspectorWrapper>();
+                _inspectors = Globals.ThisAddIn.Application.Inspectors;
+                _inspectors.NewInspector += new Outlook.InspectorsEvents_NewInspectorEventHandler(WrapInspector);
+                //MessageBox.Show("HERE 3");
+
+                // Handle also already existing Inspectors
+                // (e.g. Double clicking a .msg file)
+                foreach (Outlook.Inspector inspector in _inspectors)
+                {
+                    WrapInspector(inspector);
+                }
+            }
+            catch (Exception er) { MessageBox.Show(er.ToString()); }
+            //MessageBox.Show("HERE 4");
+        }
+
+        /// <summary>
+        /// Wraps an Inspector if required and remember it in memory to get events of the wrapped Inspector
+        /// </summary>
+        /// <param name="inspector">The Outlook Inspector instance</param>
+        void WrapInspector(Outlook.Inspector inspector) {
+            try
+            {
+                InspectorWrapper wrapper = InspectorWrapper.GetWrapperFor(inspector);
+                if (wrapper != null)
+                {
+                    // register for the closed event
+                    wrapper.Closed += new InspectorWrapperClosedEventHandler(wrapper_Closed);
+                    // remember the inspector in memory
+                    _wrappedInspectors[wrapper.Id] = wrapper;
+                }
+            }
+            catch (Exception er) { MessageBox.Show(er.ToString()); }
+        }
+
+        /// <summary>
+        /// Method is called when an inspector has been closed
+        /// Removes reference from memory
+        /// </summary>
+        /// <param name="id">The unique id of the closed inspector</param>
+        void wrapper_Closed(Guid id) {
+            try
+            {
+                _wrappedInspectors.Remove(id);
+            }
+            catch (Exception er) { MessageBox.Show(er.ToString()); }
+        }
+
+        /// <summary>
+        /// Shutdown method is called when Outlook is unloading the add-in
+        /// </summary>
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+            try
+            {
+                //MessageBox.Show("HERE 5");
+                // do the homework and cleanup
+                _wrappedInspectors.Clear();
+                _inspectors.NewInspector -= new Outlook.InspectorsEvents_NewInspectorEventHandler(WrapInspector);
+                _inspectors = null;
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+            }
+            catch (Exception er) { MessageBox.Show(er.ToString()); }
+
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
+
+
